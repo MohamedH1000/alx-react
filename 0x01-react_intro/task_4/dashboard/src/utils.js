467463272ca5c95efe02e15993ad7956637e72d8
MohamedH1000 <mohammedhisham115@gmail.com>
2024-01-04 21:07:25 +0200

    react intro

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,9 @@
+export const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+
+export const getLatestNotification = () => {
+    return (
+        <p><strong>Urgent requirement</strong> - complete by EOD</p>
+    );
+}
